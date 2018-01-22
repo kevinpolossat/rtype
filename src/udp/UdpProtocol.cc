7aaa8540ab0696fc2758ef3cefc3814f85444908
kevin <kpolossat@tellmeplus.com>
2018-01-23 00:32:28 +0100

    style(prtoocl): remove newlines

--- src/udp/UdpProtocol.cc
+++ src/udp/UdpProtocol.cc
@@ -3,14 +3,10 @@
 //
 
 #include "UdpProtocol.h"
-rtype::protocol_udp::Header::Header(int id): id(id) {
-
-}
+rtype::protocol_udp::Header::Header(int id): id(id) {}
 
 rtype::protocol_udp::Entity::Entity(
         int id, int type, int state, float x, float y):
         id(id), type(type), state(state), x(x), y (y) {}
 
-rtype::protocol_udp::Event::Event(int from, int type): from(from), type(type) {
-
-}
+rtype::protocol_udp::Event::Event(int from, int type): from(from), type(type) {}
