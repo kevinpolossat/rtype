3bf729477d6923c23582e22277f42c581daaae7f
Vincent Lelong <vincent.lelong@epitech.eu>
2018-01-28 22:28:27 +0100

    fix: getting the right game id when joining a game

--- src/client/States/JoinState.cc
+++ src/client/States/JoinState.cc
@@ -10,31 +10,29 @@ bool JoinState::Init(ge::GameEngine & engine)
 
 void JoinState::UpdateList(ge::Vector2f const & size)
 {
-	if (world_.texts.size() > 0)
+	if (!world_.texts.empty())
 		world_.texts.clear();
 	ge::MenuValue &val = ge::MenuValue::Instance();
 	_games = val.games;
-	int i = 2;
-	world_.CreateText(ge::Vector2f(size.x / 2.f, size.y / 10.f), "GAMES :", "retro",NONE, true);
+	int i = 1;
+	world_.CreateText(ge::Vector2f(size.x / 2.f, size.y / 10.f), "GAMES :", "retro", true);
 	for (auto const & it : _games)
 	{
-		world_.CreateText(ge::Vector2f(size.x / 2.f, size.y / 10.f * i), it, "retro", NONE + i - 1, true);
-		i++;
+		world_.CreateText(ge::Vector2f(size.x / 2.f, size.y / 10.f * (i + 1)), it, "retro", NONE + i, true);
+		++i;
 	}
-	world_.CreateText(ge::Vector2f(size.x / 2.f, size.y / 10.f * i + 2), "Cancel", "retro", CANCEL, true);
+	world_.CreateText(ge::Vector2f(size.x / 2.f, size.y / 10.f * (i + 1)), "Cancel", "retro", CANCEL, true);
 }
 void JoinState::HandleClickOnText_(ge::GameEngine & engine, ge::GameObject & obj) {
 	ge::MenuValue &val = ge::MenuValue::Instance();
 	switch (obj.GetComponent<ge::Input>()->id) {
-		case VALID:
-			break;
 		case CANCEL:
 			engine.PopState();
 			break;
 		case NONE:
 			break;
 		default:
-			val.j_game.value.gameId = val.gi[obj.GetComponent<ge::Input>()->id - (NONE + 1)].gameId;
+			val.j_game.value.gameId = val.gi[obj.GetComponent<ge::Input>()->id - NONE - 1].gameId;
 			val.j_game.value.port = val.Port;
 			val.tcpConnection->sendToServer(val.j_game);
 			break;
